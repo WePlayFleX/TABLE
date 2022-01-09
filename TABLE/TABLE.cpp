afbe3a4047337592b10289240ba0319b9bda994f
WePlayFleX <alex.kamai@ya.ru>
2022-01-09 21:20:59 +0300

    TABLE_V1

--- TABLE/TABLE.cpp
+++ TABLE/TABLE.cpp
@@ -3,17 +3,21 @@
 using namespace std;
 
 void Starline();
+void Repchar(char, int);
 
 int main()
 {
-	Starline();
+	Repchar('-', 43);
+	/*Starline();*/
 	cout << "Data type     Range " << endl;
-	Starline();
+	Repchar('=', 23);
+	/*Starline();*/
 	cout << "char	-128....127 " << endl
 		<< "short	-32,768....32,767 " << endl
 		<< "int		System dependent " << endl
 		<< "long	-2,147,483,648....2,147,483,647 " << endl;
-	Starline();
+	Repchar('-', 43);
+	/*Starline();*/
 
 	system("pause");
 	return 0;
@@ -24,6 +28,15 @@ void Starline()
 	for (int i = 0; i < 45; i++)
 	{
 		cout << '*';
+	}
+	cout << endl;
+}
+
+void Repchar(char ch, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << ch;
 	}
 	cout << endl;
 }
